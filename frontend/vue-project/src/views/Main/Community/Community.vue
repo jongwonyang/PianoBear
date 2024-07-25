0080cacf994b1c7ae398cb5a90778eec8bf1e34c
서근범 <seo6893@naver.com>
2024-07-25 09:16:04 +0900

    feat: 회의방 컴포넌트

--- frontend/vue-project/src/views/Main/Community/Community.vue
+++ frontend/vue-project/src/views/Main/Community/Community.vue
@@ -1,6 +1,66 @@
 <template>
-    community
+    <div>
+        <div style="position:relative;">
+            <div class="bear-frame">
+                <Bear class="bear"/>
+            </div>
+            <div class="video-frame">
+                <Video class="video"/>
+            </div>
+            <div class="btn">
+                <md-elevated-button @click="create">생성</md-elevated-button>
+            </div>
+        </div>
+        <Create v-if="createCheck" @exit="exit"/>
+    </div>
 </template>
-<script setup>
+<script lang="ts" setup>
+import {RouterView} from 'vue-router';
+import {ref} from 'vue';
+import Video from '@/components/Community/Video.vue';
+import Bear from '@/components/Community/Bear.vue';
+import Create from '@/components/Community/CreateCommunity.vue';
+
+const createCheck = ref(false);
+
+const create = function() {
+    createCheck.value = true;
+}
+
+const exit = function() {
+    createCheck.value = false;
+}
 
 </script>
+<style scoped>
+.btn {
+    position: absolute;
+    margin-left: 70%;
+    width: 200px;
+    height: 100px;
+}
+.bear-frame {
+    width: 100%;
+    height: 700px;
+    margin-top: 5%;
+    z-index: -1;
+}
+.bear {
+    position: relative;
+    width: 697px;
+    height: 700px;
+    margin: auto;
+}
+.video-frame {
+    position: absolute;
+    width: 100%;
+    height: 430px;
+    top : 170px;
+    z-index: 1;
+}
+.video {
+    position: relative;
+    margin: auto;
+    width: 410px;
+}
+</style>
