71b65d2a1ef11fffb85dd20304e77108ddee2a97
서근범 <seo6893@naver.com>
2024-07-29 17:57:41 +0900

    fix: Piano Component

--- frontend/vue-project/src/components/PianoSheet/Piano.vue
+++ frontend/vue-project/src/components/PianoSheet/Piano.vue
@@ -2,11 +2,20 @@
     <div class="piano">
         <div class="octave" v-for="oct in octaves" :key="oct">
             <div class="blacks">
-                <div class="black" v-for="(key, index) in blakcs.filter((key, index) => ((oct !== 0 && oct !== 8) || (oct === 0 && key === 'A#')))" :key="key" :class="{hide : (key === 'N')}">
+                <div class="black"
+                    v-for="(key, index) in blakcs.filter((key, index) =>
+                    ((oct !== 0 && oct !== 8) || (oct === 0 && index > 4)))"
+                    @click="keyPush(oct, key)"
+                    :key="key"
+                    :class="{hide : (key === 'N')}">
                 </div>
             </div>
             <div class="whites">
-                <div class="white" v-for="(key, index) in whites.filter((key, index) => ((oct !== 0 && oct !== 8) || (oct === 0 && index > 4) || (oct === 8 && key === 'C')))" :key="key">
+                <div class="white"
+                    v-for="(key, index) in whites.filter((key, index) =>
+                    ((oct !== 0 && oct !== 8) || (oct === 0 && index > 4) || (oct === 8 && key === 'C')))"
+                    @click="keyPush(oct, key)"
+                    :key="key">
                 </div>
             </div>
         </div>
@@ -18,6 +27,11 @@
     const octaves = ref([0, 1, 2, 3, 4, 5, 6, 7, 8]);
     const whites = ref(['C', 'D', 'E', 'F', 'G', 'A', 'B']);
     const blakcs = ref(['C#', 'D#','N', 'F#', 'G#', 'A#', 'N']);
+    const nowKey = ref<Array<String>>([]);
+
+    const keyPush = function(oct : Number, key : String) {
+        console.log(oct + " 옥타브 " + key);
+    }
 
 </script>
 <style scoped>
@@ -30,13 +44,19 @@
     padding-top: 13px;
     border-radius: 5px;
 }
+.octave {
+    display: flex;
+    position: relative;
+    height: 100%;
+}
 .blacks {
     position: absolute;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-around;
     width: 100%;
     height: 60%;
     margin: auto;
+    left: 8.5px;
     z-index: 1;
 }
 .black {
@@ -51,11 +71,6 @@
     position: relative;
     height: 80px;
     margin: auto;
-}
-.octave {
-    display: flex;
-    position: relative;
-    height: 100%;
 }
 .white {
     background-color: white;
