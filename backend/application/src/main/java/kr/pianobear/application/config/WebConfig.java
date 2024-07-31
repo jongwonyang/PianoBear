5804da7127409cc9f8ef003bb05e71ffa7b27913
Chansu Park <1265pcs@gmail.com>
2024-07-31 14:12:06 +0900

    fix: 2단계이상 URI에서 SPA 라우팅 안되는 문제 수정

--- backend/application/src/main/java/kr/pianobear/application/config/WebConfig.java
+++ backend/application/src/main/java/kr/pianobear/application/config/WebConfig.java
@@ -1,7 +1,5 @@
 package kr.pianobear.application.config;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,14 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-    }
-
-    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:[^.]*}")
-                .setViewName("forward:/");
+        registry.addViewController("/{spring:[^\\.]+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:[^\\.]+}")
+                .setViewName("forward:/index.html");
     }
 }
