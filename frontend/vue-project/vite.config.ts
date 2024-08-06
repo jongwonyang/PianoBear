280fe3c23a53cf27749f483696286e9473cc7574
Chansu Park <1265pcs@gmail.com>
2024-08-07 05:10:04 +0900

    feat: 프론트엔드 도커화, Windows 배치파일 제거

--- frontend/vue-project/vite.config.ts
+++ frontend/vue-project/vite.config.ts
@@ -6,7 +6,6 @@ import VueDevTools from "vite-plugin-vue-devtools";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../../backend/application/src/main/resources/static",
     emptyOutDir: true, // also necessary
   },
   plugins: [
